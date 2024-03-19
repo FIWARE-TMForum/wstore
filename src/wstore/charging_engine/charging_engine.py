237390fc6e28f1229795a9d70e2108d41b2c552a
Francisco de la Vega <fdelavega@ficodes.com>
2024-03-19 13:22:33 +0100

    Update unit tests (#87)
    * Update ordering API tests
    * Update ordering client tests
    * Update payment conf tests
    * Update ordering management tests
    * Update inventory API tests
    * Update inventory client tests
    * Fix offering validator tests
    * Update charging engine tests
    * Execute the tests in the pipelines

--- src/wstore/charging_engine/charging_engine.py
+++ src/wstore/charging_engine/charging_engine.py
@@ -266,12 +266,12 @@ class ChargingEngine:
             # Send the charge to the billing API to allow user accesses
             if concept != "initial":
                 # When the change concept is initial, the product has not been yet created in the inventory
-                # billing_client.create_charge(
-                #     charge,
-                #     contract.product_id,
-                #     start_date=valid_from,
-                #     end_date=valid_to,
-                # )
+                billing_client.create_charge(
+                    charge,
+                    contract.product_id,
+                    start_date=valid_from,
+                    end_date=valid_to,
+                )
                 pass
 
         for free in free_contracts:
