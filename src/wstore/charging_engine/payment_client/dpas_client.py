c30575c043c920971b5751f917594ef558d0cca7
gergely.fabian <gergely.fabian@egroup.hu>
2025-02-25 16:12:28 +0100

    New version for dpas payment.

--- src/wstore/charging_engine/payment_client/dpas_client.py
+++ src/wstore/charging_engine/payment_client/dpas_client.py
@@ -4,9 +4,8 @@ from django.conf import settings
 
 import os
 import requests
-from decimal import Decimal
-from logging import getLogger
 
+from logging import getLogger
 logger = getLogger("wstore.default_logger")
 
 DPAS_CLIENT_API_URL = os.environ.get("BAE_CB_DPAS_CLIENT_API_URL")
@@ -16,40 +15,63 @@ class DpasClient(PaymentClient):
 
     def __init__(self, order):
         self._order = order
-        self.api_url = DPAS_CLIENT_API_URL 
+        self.api_url = DPAS_CLIENT_API_URL
 
-    def start_redirection_payment(self, transactions):       
-        total = Decimal("0")
-        current_curr = transactions[0]["currency"]
+    def start_redirection_payment(self, transactions):     
+        payment_items = []
         for t in transactions:
-            if t["currency"] != current_curr:
-                raise PaymentError("The payment cannot be created: Multiple currencies")
-            total += Decimal(t["price"])
+            payment_item = {
+                "productProviderId": "1", #
+                "amount": float(t['price']),
+                "currency": t['currency'],
+                "productProviderSpecificData": {}
+            }
+            if "subscription" in t['related_model']:
+                payment_item.update({"recurring": True})
+            else:
+                payment_item.update({"recurring": False})
+            payment_items.append(payment_item)
+
+
+        url = settings.SITE
+        if url[-1] != "/":
+            url += "/"
+
+        success_url = url + "payment?action=accept&ref=" + str(self._order.pk)
+        cancel_url = url + "payment?action=cancel&ref=" + str(self._order.pk)
+
+        if not self._order.owner_organization.private:
+            success_url += "&organization=" + self._order.owner_organization.name
+            cancel_url += "&organization=" + self._order.owner_organization.name
 
         payload = {
-            "externalId": str(self._order.order_id),
-            "customerId": self._order.customer_id, 
-            "customerOrganizationId": str(self._order.owner_organization_id),
-            "type": "ONETIME", # recurring payment type is not yet implemented in the Dpas API
-            "invoiceId": "invoice id", # should be the order's invoice ID
-            "paymentItems": [{
-                "productProviderId": "1", # should be the product provider's ID
-                "amount": total,
-                "currency": current_curr,
-                "recurring" : False,
-                "productProviderSpecificData": {}
-            }]
+            "baseAttributes": {
+                "externalId": str(self._order.order_id),
+                "customerId": str(self._order.customer_id), 
+                "customerOrganizationId": str(self._order.owner_organization_id),
+                "invoiceId": "invoice id", #
+                "paymentItems": payment_items
+            },
+            "processSuccessUrl": success_url,
+            "processErrorUrl": cancel_url,
+            "responseUrlJwtQueryName": "jwt"
+        }
+
+        headers = {
+            "Authorization": "Bearer " + "token_placeholder" # Needs to be filled
         }
 
         try:
-            response = requests.post(self.api_url, json=payload)
+            response = requests.post(self.api_url, json=payload, headers=headers)
             self._checkout_url = response.json()["redirectUrl"]
 
         except requests.RequestException as e:
             logger.debug(f"Error contacting payment API: {e}")
 
-    def end_redirection_payment(self, token, payer_id):
-        pass
+
+    def end_redirection_payment(self, **kwargs):
+        sales_ids = ['4U7754848E5906029'] # not implemented yet
+        return sales_ids
 
     def refund(self, sale_id):
         pass
